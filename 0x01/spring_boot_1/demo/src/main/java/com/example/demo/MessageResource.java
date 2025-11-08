20a7881fbb2e72d5d492a0ed264b1099a9339ff7
Brendon <you@example.combrendon_gama@hotmail.com>
2025-11-07 21:46:18 -0300

    teste

--- 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,20 @@ public class MessageResource {
 	@GetMapping("/simpleMessageWelcome")
     public String simpleMessageWelcome() {
         return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+	
+	@GetMapping("/login")
+    public String login(@RequestParam(required = false) String user,
+                        @RequestParam(required = false) String password) {
+
+        if (user == null || password == null || user.isBlank() || password.isBlank()) {
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        }
+
+        if (user.length() > 15 || password.length() > 15) {
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        }
+
+        return "LOGIN EFETUADO COM SUCESSO !!!";
     }
 }
